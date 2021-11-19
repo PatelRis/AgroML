a08f449cb670d1465633572fb5a637b93604b580
Rohan <76815017+soft1ce@users.noreply.github.com>
2021-11-19 14:12:29 +0530

    added status check

--- encode_fix.sh
+++ encode_fix.sh
@@ -1,3 +1,9 @@
+#!bin/bash
+
+echo "Original\n"
+file -bi CropDesc.csv
+file -bi disease_info.csv
+
 iconv -c -t ascii CropDesc.csv -o newC.csv
 rm -f CropDesc.csv
 mv newC.csv CropDesc.csv
@@ -5,5 +11,9 @@ mv newC.csv CropDesc.csv
 iconv -c -t ascii disease_info.csv -o newD.csv
 rm -f disease_info.csv
 mv newD.csv disease_info.csv
+
+echo "PostScript\n"
+file -bi CropDesc.csv
+file -bi disease_info.csv
 
 
